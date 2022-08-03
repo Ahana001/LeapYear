457ce500e96407913ab8eced6e12b099be40b83c
Ankita <50478681+Ahana001@users.noreply.github.com>
2022-08-03 20:35:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var readlineSync = require('readline-sync');
 var chalk = require('chalk');
 
 var userName = readlineSync.question(chalk.blue("Enter Your Name : "));
-console.log(chalk.green("welcome "+ userName));
+console.log(chalk.green("welcome " + userName));
 
 var dateOfBirth = readlineSync.question(chalk.blue('Enter your birth-date in DD/MM/YYYY format : '));
 
@@ -10,25 +10,22 @@ let userDD = "";
 let userMM = "";
 let userYYYY = "";
 
-function leapYear(year)
-{
-  if( (year % 400 == 0 && year % 100 != 0 ) || year % 4 == 0)
-  {
+function leapYear(year) {
+  if ((year % 400 == 0 && year % 100 != 0) || year % 4 == 0) {
     console.log(chalk.green.underline.bold(userName + "!! your birth year is leap year"));
-  }else
-  {
+  } else {
     console.log(chalk.redBright.underline.bold(userName + "!! your birth year is not leap year"));
   }
 }
 
-function validateDOB(date){
-  userDD = date.substring(0,2);
-  userMM = date.substring(3,5);
-  userYYYY = date.substring(6);
+function validateDOB(date) {
+  userDD = date.substring(0, 2);
+  userMM = date.substring(3, 5);
+  userYYYY = date.substring(6, 10);
 
-  if(isNaN(userDD) ||  isNaN(userMM) || isNaN(userYYYY)){
-    console.log(console.log(chalk.red("please enter valid birth date")));
-  }else{
+  if (isNaN(userDD) || isNaN(userMM) || isNaN(userYYYY) || 0 > Number(userDD)|| Number(userDD)>31 || 0 > Number(userMM) || Number(userMM) >13 || Number(userYYYY) < 0) {
+    console.log(console.log(chalk.red("please enter valid birth date 😡 ")));
+  }else {
     leapYear(userYYYY);
   }
 }
